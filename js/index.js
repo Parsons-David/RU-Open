ec6130028cb51eaa327fd3ff15b4650325a42330
Parsons-David <david.parsons@rutgers.edu>
2017-05-26 14:27:07 -0400

    Implement Objects for Dynamic Page Creation

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var index;
 
 function onLoad(){
   console.log("Loading");
-  $.getJSON('data.json', function(data) {
+  $.getJSON('http://parsonssoftware.me/RU-Open/data.json', function(data) {
         index = new Page(data);
   });
   $("*").css( 'cursor', 'pointer' );
@@ -19,55 +19,53 @@ function onLoad(){
 
 function onClick(){
   var elem = event.target || event.srcElement;
-  elem.style.color = "green";
-  // document.getElementById('camp' + pos).style.display = (index.selectedCampuses[pos] ? "none" : '');
-  // index.selectedCampuses[pos] = !index.selectedCampuses[pos];
+  $("body").data(elem.id).toggleSelected();
 }
-
-function createCampus(name){
-  var sec = document.createElement("SECTION");
-  sec.innerHTML = name;
-  sec.onclick = onClick;
-  return sec;
-}
-
-function createBuilding(name){
-  var sec = document.createElement("div");
-  sec.innerHTML = '&emsp;' + name;
-  sec.onclick = onClick;
-  // sec.style.display = 'none';
-  // sec.onclick = onCampusClick;
-  return sec;
-}
-
-function createRoom(name){
-  var sec = document.createElement("div");
-  sec.innerHTML = '&emsp;&emsp;' + name;
-  sec.onclick = onClick;
-  // sec.style.display = 'none';
-  // sec.onclick = onCampusClick;
-  return sec;
-}
-
-function getBuildings(campus){
-  var div = document.createElement("div");
-  div.id = 'div' + campus;
-  for(var b in campus){
-    div.append(createBuilding(b));
-    div.append(getRooms(campus[b]));
-  }
-  return div;
-}
-
-function getRooms(build){
-  var div = document.createElement("div");
-  div.id = 'div' + build;
-  for(var r in build){
-    div.append(createRoom(r));
-    // div.append(getRooms(build[r]));
-  }
-  return div;
-}
+//
+// function createCampus(name){
+//   var sec = document.createElement("SECTION");
+//   sec.innerHTML = name;
+//   sec.onclick = onClick;
+//   return sec;
+// }
+//
+// function createBuilding(name){
+//   var sec = document.createElement("div");
+//   sec.innerHTML = '&emsp;' + name;
+//   sec.onclick = onClick;
+//   // sec.style.display = 'none';
+//   // sec.onclick = onCampusClick;
+//   return sec;
+// }
+//
+// function createRoom(name){
+//   var sec = document.createElement("div");
+//   sec.innerHTML = '&emsp;&emsp;' + name;
+//   sec.onclick = onClick;
+//   // sec.style.display = 'none';
+//   // sec.onclick = onCampusClick;
+//   return sec;
+// }
+//
+// function getBuildings(campus){
+//   var div = document.createElement("div");
+//   div.id = 'div' + campus;
+//   for(var b in campus){
+//     div.append(createBuilding(b));
+//     div.append(getRooms(campus[b]));
+//   }
+//   return div;
+// }
+//
+// function getRooms(build){
+//   var div = document.createElement("div");
+//   div.id = 'div' + build;
+//   for(var r in build){
+//     div.append(createRoom(r));
+//     // div.append(getRooms(build[r]));
+//   }
+//   return div;
+// }
 
 class Page{
   constructor(data){
@@ -75,31 +73,114 @@ class Page{
     this.json = data;
 
     for(var k in this.json){
-      document.getElementById("body").append(createCampus(k));
-      document.getElementById("body").append(getBuildings(this.json[k]));
-      console.log(k);
+      $("body").data(k, new Campus(k, this.json[k]));
+      document.getElementById("body").append($("body").data(k).sec);
+      document.getElementById("body").append($("body").data(k).div);
+      // document.getElementById("body").append(getBuildings(this.json[k]));
+      console.log(k + " -> " + this.json[k]);
     }
 
-    this.selectedCampuses = [];
-    for (var i = 0; i < 5; i++) this.selectedCampuses[i] = false;
+    // this.selectedCampuses = [];
+    // for (var i = 0; i < 5; i++) this.selectedCampuses[i] = false;
   }
 }
 
 class Campus{
-  constructor(name){
+  constructor(name, buildings){
     // Campus Name (Livi, College Ave, etc.)
     this.name = name;
     // Is campus used in search
     this.selected = false;
+
+    // Document Elements
+    // Section for Campus
+    this.sec = document.createElement("SECTION");
+    this.sec.id = name;
+    this.sec.innerHTML = name;
+    this.sec.onclick = onClick;
+
+    // Div for Buildings on Campus
+    this.div = document.createElement("div");
+    this.div.style.display = 'none';
+
+    for(var b in buildings){
+      $("body").data(b, new Building(b, buildings[b]))
+      this.div.append($("body").data(b).sec);
+      this.div.append($("body").data(b).div);
+    }
+
+  }
+
+  toggleSelected(){
+    this.selected = !this.selected;
+    this.sec.style.color = (this.selected ? 'green' : '' );
+    this.div.style.display = (this.selected ? '' : 'none');
   }
 
 }
 
 class Building{
-  constructor(name){
+  constructor(name, room){
     // Building Name (Hill Center, Loree, etc)
     this.name = name;
     // Is Building included in search
-    this.selected = true;
+    this.selected = false;
+
+    // Document Elements
+    // Section for Campus
+    this.sec = document.createElement("SECTION");
+    this.sec.id = name;
+    this.sec.innerHTML = '&emsp;' + name;
+    this.sec.onclick = onClick;
+
+    // Div for Buildings on Campus
+    this.div = document.createElement("div");
+    this.div.style.display = 'none';
+
+    for(var r in room){
+      $("body").data(r, new Room(r, room[r]))
+      this.div.append($("body").data(r).sec);
+      this.div.append($("body").data(r).div);
+    }
+  }
+
+  toggleSelected(){
+    this.selected = !this.selected;
+    this.sec.style.color = (this.selected ? 'green' : '' );
+    this.div.style.display = (this.selected ? '' : 'none');
+  }
+}
+
+
+class Room{
+  constructor(name, time){
+    // Building Name (Hill Center, Loree, etc)
+    this.name = name;
+    // Is Building included in search
+    this.selected = false;
+
+    // Document Elements
+    // Section for Campus
+    this.sec = document.createElement("SECTION");
+    this.sec.id = name;
+    this.sec.innerHTML = '&emsp;&emsp;' + name;
+    this.sec.onclick = onClick;
+
+    // Div for Buildings on Campus
+    this.div = document.createElement("div");
+    this.div.style.display = 'none';
+
+    // for(var r in room){
+    //   $("body").data(r, new Room(r, room[r]))
+    //   this.div.append($("body").data(r).sec);
+    //   this.div.append($("body").data(r).div);
+    // }
+  }
+
+  toggleSelected(){
+    this.selected = !this.selected;
+    this.sec.style.color = (this.selected ? 'green' : '' );
+      console.log("Room Selected");
+    this.div.style.display = (this.selected ? '' : 'none');
   }
 }
