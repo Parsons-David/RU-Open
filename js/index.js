cd96ca08d7ba46a6ccebd618de5353753fcf6067
Parsons-David <david.parsons@rutgers.edu>
2017-05-25 20:24:30 -0400

    Add div

--- js/index.js
+++ js/index.js
@@ -3,18 +3,28 @@ var index;
 function onLoad(){
   index = new Page();
   console.log("Loading");
+  for (var i = 0; i < 5; i++){
+    var div = document.getElementById('camp' + i);
+    div.style.display = 'none';
+    for(var j = 1; j < 11; j++){
+      var sec = document.createElement("SECTION");
+      sec.innerHTML = '&emsp;Test' + j;
+      div.append(sec);
+    }
+  }
 }
 
 function onCampusClick(pos){
   var campus = event.target || event.srcElement;
   campus.style.color = (index.selectedCampuses[pos] ? "" : "green");
+  document.getElementById('camp' + pos).style.display = (index.selectedCampuses[pos] ? "none" : '');
   index.selectedCampuses[pos] = !index.selectedCampuses[pos];
 }
 
 class Page{
   constructor(){
     this.selectedCampuses = [];
-    for (var i = 0; i < 4; i++) this.selectedCampuses[i] = false;
+    for (var i = 0; i < 5; i++) this.selectedCampuses[i] = false;
   }
 }
 
